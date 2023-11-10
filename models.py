d19d983b36eb85e523e9600d79c3d0b453291af8
EDG-Yiper <144791454+EDG-Yiper@users.noreply.github.com>
2023-11-10 21:23:52 +0800

    Add files via upload

--- models.py
+++ models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 
 # print("nihao")
+class UserInfo(models.Model):
+    name = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    age = models.IntegerField()
